bf84f669bf96ab5ed589ee0dfe05b1662427ae97
David Carr <dave@daveismyname.com>
2017-03-13 19:44:16 +0000

    Update BackendController.php

--- app/Core/BackendController.php
+++ app/Core/BackendController.php
@@ -52,7 +52,7 @@ abstract class BackendController extends BaseController
      * While this method is supposed to setup the Controller's Layout instance, it could be
      * used well as method which is always executed before the current action, aka before()
      */
-    protected function setupLayout()
+    protected function before()
     {
         if (Auth::check()) {
             // The User is logged in; setup the Backend Menu.
