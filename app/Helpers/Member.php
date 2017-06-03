f585b63c807db8baf9bad6a85f4888a131ba9584
David Carr <dave@daveismyname.com>
2017-06-03 13:47:32 +0100

    reverted member helper

--- app/Helpers/Member.php
+++ app/Helpers/Member.php
@@ -5,7 +5,6 @@ use App\Modules\System\Models\User;
 use App\Modules\System\Models\UserLog;
 
 use Auth;
-use Config;
 use Redirect;
 use Request;
 use Session;
@@ -25,41 +24,24 @@ class Member
 
             Auth::logout();
 
-            return Redirect::to('')->withStatus('Your account is no longer active.', 'danger');
+            // User not activated; go logout and redirect him back.
+            return Redirect::to()->withStatus('Your account is no longer active.', 'danger');
         }
 
-        if ($user->officeLoginOnly == 'Yes'){
+        //if ($user->role_id != 1) {
 
-            $allowlist = Config::get('app.ipAccessList');
+		    if (Session::get('changelogin') == true && Request::path() !='users/'.$user->id.'/edit') {
+	    		header('Location: '.site_url('users/'.$user->id.'/edit'));
+				exit;
+			}
 
-            if (!array_key_exists($_SERVER['REMOTE_ADDR'], $allowlist)) {
+			if ($user->forceChangePassword == 'Yes' && Request::path() !='users/'.$user->id.'/edit') {
+	            Session::set('changelogin', true);
+	            header('Location: '.site_url('users/'.$user->id.'/edit'));
+				exit;
+	        }
 
-                $log          = new UserLog();
-                $log->user_id = Auth::user()->id;
-                $log->title   = "Attempted to login from an unauthorised location. User set to office login only.";
-                $log->section = "users";
-                $log->refID   = $user->id;
-                $log->type    = 'Auth';
-                $log->save();
-
-                Auth::logout();
-
-                return Redirect::to('')->withStatus('This system cannot be accessed from your location.', 'danger');
-            }
-
-        }
-
-
-	    if (Session::get('changelogin') == true && Request::path() !='users/'.$user->id.'/edit') {
-    		header('Location: '.site_url('users/'.$user->id.'/edit'));
-			exit;
-		}
-
-		if ($user->forceChangePassword == 'Yes' && Request::path() !='users/'.$user->id.'/edit') {
-            Session::set('changelogin', true);
-            header('Location: '.site_url('users/'.$user->id.'/edit'));
-			exit;
-        }
+	    //}
 
 	}
 
@@ -72,16 +54,16 @@ class Member
 	 */
 	public static function get($column, $id = null)
 	{
-        if ($id == null) {
-            if (Auth::check()) {
-                $id = Auth::user();
-            }
-        }
+		//if user is logged in
+        if (Auth::check()) {
+			if ($id === null) {
+				return Auth::user()->$column;
+			}
 
-        $user = self::getData($id);
+			$user = self::getData($id);
 
-        return $user->$column;
-
+		    return $user->$column;
+		}
 	}
 
     /**
