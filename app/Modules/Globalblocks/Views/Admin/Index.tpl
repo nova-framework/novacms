3d395db7eff3486d19301223855e64f44e3c3ec7
David Carr <dave@daveismyname.com>
2017-06-03 21:43:20 +0100

    removed toggle icons to boxes

--- app/Modules/Globalblocks/Views/Admin/Index.tpl
+++ app/Modules/Globalblocks/Views/Admin/Index.tpl
@@ -13,9 +13,6 @@
 <div class="box box-primary">
     <div class="box-header with-border">
         <h3 class="box-title">Global Blocks</h3>
-        <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-        </div>
     </div>
     <div class="box-body">
 
--- app/Modules/System/Views/Depts/Index.php
+++ app/Modules/System/Views/Depts/Index.php
@@ -46,10 +46,6 @@
 		<div class="box box-primary">
             <div class="box-header with-border">
                 <h3 class="box-title">Depts</h3>
-                <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                </div>
             </div>
             <div class="box-body">
 
