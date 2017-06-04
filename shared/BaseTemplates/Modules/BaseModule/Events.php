0618bb8b9c207d18688715a215a45112bd35e93d
David Carr <dave@daveismyname.com>
2017-06-04 21:07:26 +0100

    Added baseModule events

--- shared/BaseTemplates/Modules/BaseModule/Events.php
+++ shared/BaseTemplates/Modules/BaseModule/Events.php
@@ -1,22 +1,26 @@
 <?php
-/**
- * Events - all Module's specific Events are defined here.
- *
- */
+Event::listen('backend.menu.quickcreate', function($user) {
+    $items = array(
+        array(
+            'uri'    => 'BaseModuleSlug/create',
+            'title'  => __d('BaseModuleSlug', 'Create BaseModuleTitle'),
+            'icon'   => 'cubes',
+            'weight' => 1,
+        ),
+    );
+
+    return $items;
+});
 
 Event::listen('backend.menu.modules', function($user) {
-    if ($user->hasRole('administrator')) {
-        $items = array(
-            array(
-                'uri'    => 'BaseModuleSlug',
-                'title'  => __d('BaseModuleSlug', 'BaseModuleTitle'),
-                'icon'   => 'cubes',
-                'weight' => 1,
-            ),
-        );
-    } else {
-        $items = array();
-    }
+    $items = array(
+        array(
+            'uri'    => 'BaseModuleSlug',
+            'title'  => __d('BaseModuleSlug', 'BaseModuleTitle'),
+            'icon'   => 'cubes',
+            'weight' => 1,
+        ),
+    );
 
     return $items;
 });
